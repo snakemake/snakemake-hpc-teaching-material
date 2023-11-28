0df1c7ae5a5fb3400d36d2e341c8747bda580a9c
Christian Meesters <meesters@uni-mainz.de>
2023-11-28 15:23:08 +0100

    add 'copy_script_mogonII.sh' as an example, tested #12

---
+++ setup/copy_script_mogonII.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is used to teach on the cluster "Mogon II"
+
+CLUSTER_ALIAS="mogon_nox" # nox stands for "no X11 forwarding"
+BASEPATH="/lustre/project/hpckurs/workflows" # repeated used
+
+# creating remote directory:
+ssh mogon_nox "mkdir -p ${BASEPATH}"
+
+scp condarc "${CLUSTER_ALIAS}:/lustre/project/hpckurs/workflows/condarc"
+scp get_tutorial.sh "${CLUSTER_ALIAS}:/lustre/project/hpckurs/workflows/get_tutorial.sh"
+
+rsync -rtlv --chmod=D755 "tutorial" "${CLUSTER_ALIAS}:/lustre/project/hpckurs/workflows/tutorial"
+rsync -rtlv --chmod=D755 "solutions" "${CLUSTER_ALIAS}:/lustre/project/hpckurs/solutions"
