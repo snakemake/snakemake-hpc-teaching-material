b6e5babbfa9329a39bf7d19c44c54a703745298f
Christian Meesters <meesters@uni-mainz.de>
2024-10-16 21:24:33 +0200

    style: blacked previous commit

--- setup_creators/solutions/scripts/plot-positions.py
+++ setup_creators/solutions/scripts/plot-positions.py
@@ -1,5 +1,6 @@
 import matplotlib
-matplotlib.use("Agg") # to suppress interactive plotting
+
+matplotlib.use("Agg")  # to suppress interactive plotting
 import matplotlib.pyplot as plt
 import numpy as np
 from pysam import VariantFile
@@ -8,20 +9,20 @@ import seaborn as sns
 window_size = 500
 
 pos = [record.pos for record in VariantFile(snakemake.input[0])]
-# setup windows 
+# setup windows
 bins = np.arange(0, max(pos), window_size)
-    
+
 # use window midpoints as x coordinate
-x = (bins[1:] + bins[:-1])/2
-    
+x = (bins[1:] + bins[:-1]) / 2
+
 # compute variant density in each window
 h, _ = np.histogram(pos, bins=bins)
 y = h / window_size
-    
+
 # plot
 fig, ax = plt.subplots(figsize=(12, 3))
-sns.despine(ax=ax)#, offset=10)
+sns.despine(ax=ax)  # , offset=10)
 ax.plot(x, y)
-ax.set_xlabel('Chromosome position (bp)')
-ax.set_ylabel('Variant density (bp$^{-1}$)')
+ax.set_xlabel("Chromosome position (bp)")
+ax.set_ylabel("Variant density (bp$^{-1}$)")
 plt.savefig(snakemake.output[0])
