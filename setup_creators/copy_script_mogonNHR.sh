3a41ef691c1df9d5e592aa77b78699afd439f748
Christian Meesters <meesters@uni-mainz.de>
2025-12-09 07:44:39 +0100

    fix: diverse

--- setup_creators/copy_script_mogonNHR.sh
+++ setup_creators/copy_script_mogonNHR.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# This script is used to teach on the cluster "Mogon II"
+# This script is used to teach on the cluster "Mogon KI"
 
-CLUSTER_ALIAS="mogon_NHR_nox" # nox stands for "no X11 forwarding"
-BASEPATH="/lustre/project/nhr-workflow/tutorial" # repeated used
+CLUSTER_ALIAS="mogon-nhr" # nox stands for "no X11 forwarding"
+BASEPATH="/lustre/project/ki-workflow/" # repeated used
 
 # creating remote directory:
-ssh ${CLUSTER_ALIAS} "mkdir -p ${BASEPATH}"
+ssh ${CLUSTER_ALIAS} "mkdir -p ${BASEPATH}/snakemake_profile"
 
 scp condarc_mogon "${CLUSTER_ALIAS}:${BASEPATH}/condarc"
 scp get_tutorial.sh "${CLUSTER_ALIAS}:${BASEPATH}/get_tutorial.sh"
 scp install_micromamba.sh "${CLUSTER_ALIAS}:${BASEPATH}/install_conda.sh"
-scp environment.yaml "${CLUSTER_ALIAS}:${BASEPATH}/environment.yaml"
+scp environment.yaml "${CLUSTER_ALIAS}:."
+scp config.yaml "${CLUSTER_ALIAS}:${BASEPATH}/snakemake_profile/."
 
 rsync -rtlv --chmod=D755 "tutorial" "${CLUSTER_ALIAS}:${BASEPATH}"
-rsync -rtlv --chmod=D755 "solutions" "${CLUSTER_ALIAS}:/lustre/project/nhr-worfklow"
+rsync -rtlv --chmod=D755 "solutions" "${CLUSTER_ALIAS}:${BASEPATH}"
